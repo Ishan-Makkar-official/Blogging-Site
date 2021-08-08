40d3c6194a46bf67f471f5c53cda35686a497101
Ishan Makkar <ishanmakkar3@gmail.com>
2021-08-08 13:08:25 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const { first } = require("lodash");
+const _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "It is a Blog or Daily journal where you can delete or add the posts and have a track of your daily routine.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts=[];
+const posts=[{title:"po",content:"hi"}];
 
 
 
@@ -43,10 +43,49 @@ app.get("/compose",function(req,res)
   res.render("compose");
 })
 
+app.get("/edit",function(req,res)
+{
+  res.render("edit");
+})
+
+
+app.get("/posts/:topic",function(req,res)
+{
+  const requestedTitle=_.lowerCase(req.params.topic);
+  var i=0;
+  for(i=0;i<posts.length;i++)
+  {
+    var k=_.lowerCase(posts[i].title);
+    if(k===requestedTitle)
+    {
+      res.render("post",{title:posts[i].title,content:posts[i].content});
+      break;
+    }
+  }
+
+  if(i==posts.length)
+  {
+    res.send("Match Not Found");
+  }
+  
+  
+
+
+
+
+
+
+
+
+
+
+});
+
+
 app.post("/compose",function(req,res)
 {
   var n=req.body.title;
-  var k=req.body.post
+  var k=req.body.post;
   
   const newPost={
     title:n,
@@ -58,6 +97,39 @@ app.post("/compose",function(req,res)
 
 
 })
+
+app.post("/edit",function(req,res)
+{
+  var n=req.body.title;
+  var k=req.body.post;
+
+  var i=0;
+  for(;i<posts.length;i++)
+  {
+    if(posts[i].title==n)
+    {
+      posts[i].content=k;
+      break;
+    }
+  }
+
+  const newPost={
+    title:n,
+    content:k
+  };
+
+  if(i==posts.length)
+  {
+    posts.push(newPost);
+  }
+
+  res.redirect("/");
+
+
+
+})
+
+
 
 
 
