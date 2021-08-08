3adf3c1751fc811e605d5dc1b6b010e920357612
Ishan Makkar <ishanmakkar3@gmail.com>
2021-08-08 18:52:28 +0530

    Third Commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ejs = require("ejs");
 const _ = require("lodash");
 
 const homeStartingContent = "It is a Blog or Daily journal where you can delete or add the posts and have a track of your daily routine.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const aboutContent = "It is a Blog or Daily Journal where you can add delete update the posts  and have a track of your daily routine";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts=[{title:"po",content:"hi"}];
+const posts=[];
 
 
 
@@ -81,6 +81,11 @@ app.get("/posts/:topic",function(req,res)
 
 });
 
+app.get("/remove",function(req,res)
+{
+  res.render("remove");
+})
+
 
 app.post("/compose",function(req,res)
 {
@@ -113,20 +118,26 @@ app.post("/edit",function(req,res)
     }
   }
 
-  const newPost={
-    title:n,
-    content:k
-  };
-
-  if(i==posts.length)
-  {
-    posts.push(newPost);
-  }
-
   res.redirect("/");
 
 
 
+})
+
+app.post("/remove",function(req,res)
+{
+  var n=req.body.title;
+  var i=0;
+  for(;i<posts.length;i++)
+  {
+    if(posts[i].title===n)
+    {
+      var k=posts.splice(i,1);
+      break;
+    }
+  }
+
+  res.redirect("/");
 })
 
 
